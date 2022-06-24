0177fc7436a56abb118ed139eff1995649d0a8c5
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-06-24 18:38:44 +0430

    Update: courses module admin

--- courses/admin.py
+++ courses/admin.py
@@ -4,17 +4,39 @@ from .models import Subject, Course, Chapter, Content
 # Register your models here.
 
 
+class ChapterInline(admin.StackedInline):
+    model = Chapter
+
+
+class ContentInline(admin.StackedInline):
+    model = Content
+
+
 class SubjectAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug')
 
 
 class CourseAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'owner')
+    inlines = [ChapterInline]
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'course')
+    inlines = [ContentInline]
+
+
+class ContentAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'chapter')
 
 
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Chapter)
-admin.site.register(Content)
+admin.site.register(Chapter, ChapterAdmin)
+admin.site.register(Content, ContentAdmin)
