1b79711eb8d2d18e1d0d3c7682adec6184d5f80d
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-07-09 20:03:31 +0430

    Add: CourseListSerializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from dataclasses import field
 from rest_framework import serializers
-from courses.models import Subject
+from courses.models import Subject, Course
 
 
 class SubjectListSerializer(serializers.ModelSerializer):
@@ -15,3 +15,24 @@ class SubjectListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = '__all__'
+
+
+class CourseListSerializer(serializers.ModelSerializer):
+    chapters = serializers.SerializerMethodField(method_name='get_chapters')
+    subjects = serializers.SerializerMethodField(method_name='get_subjects')
+
+    def get_chapters(self, obj):
+        chapters = [
+            chapter.title for chapter in obj.chapters.get_queryset().only('title')
+        ]
+        return chapters
+
+    def get_subjects(self, obj):
+        subjects = [
+            subject.title for subject in obj.subject.get_queryset().only('title')
+        ]
+        return subjects
+
+    class Meta:
+        model = Course
+        fields = ('owner', 'subjects', 'title', 'description', 'chapters')
