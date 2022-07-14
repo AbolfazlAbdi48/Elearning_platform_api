2eabd3deae0caf30b55a0e0829ed99200f9acb80
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-07-14 12:49:19 +0430

    Add: IsSuperUser, IsSuperUserOrOwner permissions

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,10 +1,26 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsSuperUserOrTeacher(permissions.BasePermission):
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_authenticated and request.user.is_superuser)
+
+
+class IsSuperUserOrTeacher(BasePermission):
     message = 'you must be superuser or teacher.'
 
     def has_permission(self, request, view):
+        return bool(
+            request.user.is_authenticated and request.user.is_superuser or
+            request.user.is_authenticated and request.user.is_staff
+        )
+
+
+class IsSuperUserOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
         return bool(
             request.user.is_authenticated and request.user.is_superuser or
             request.user.is_authenticated and request.user.is_staff
