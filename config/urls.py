4b1dc521260f1964ce2ca8c2f584969764721e9a
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-07-15 15:41:06 +0430

    Add: swagger

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf.urls.static import static
 from django.conf import settings
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="E-learning Platform API",
+      default_version='v1',
+      description="E-learning Platform API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('api/rest-auth/', include('dj_rest_auth.urls')),
@@ -25,6 +43,10 @@ urlpatterns = [
 
     path('', include('teachers.urls')),
     path('', include('courses.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
 ]
